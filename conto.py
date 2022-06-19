641b0537a6a5293b732269535f786f880e5d4afe
Andrea <82764646+Dozzy85@users.noreply.github.com>
2022-06-19 21:25:56 +0200

    Esercitazione

--- conto.py
+++ conto.py
@@ -1,27 +1,36 @@
-class ContoCorrente:
+#La classe Conto è una super classe
+class Conto:
+    def __init__(self, nome, conto):
+        self.nome=nome
+        self.conto=conto
+
+#Classe ContoCorrente è una sotto classe della classe Conto
+class ContoCorrente(Conto):
     def __init__(self, nome, conto, importo):
-        self.nome = nome
-        self.conto = conto
-        self.saldo = importo
+        super().__init__(nome,conto)
+        self.__saldo = importo
 
     def preleva(self, importo):
-        self.saldo -= importo
+        self.__saldo -= importo
 
     def deposita(self, importo):
-        self.saldo += importo
+        self.__saldo += importo
 
     def descrizione(self):
-        print(self.nome, self.conto, self.saldo)
+        print(self.nome, self.conto, self.__saldo)
 
+    #property per la lettura del dato reso "privato" tramite le __
+    @property
+    def saldo(self):
+        print("Sono dentro getter")
+        return self.__saldo
+
+    # property per la scrittura del dato reso "privato" tramite le __
+    @saldo.setter
+    def saldo(self,importo):
+        print("Sono dentro setter")
+        self.preleva(self.__saldo)
+        self.deposita(importo)
 
 c1 = ContoCorrente("Alessandro", "10", 2000)
-c2 = ContoCorrente("Susanna", "20", 5000)
-
 c1.descrizione()
-c2.descrizione()
-
-c1.deposita(500)
-c1.descrizione()
-
-c2.preleva(200)
-c2.descrizione()
